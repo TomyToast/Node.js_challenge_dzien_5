c20084431dfb59040f33bfd8bfe24c759ea9231b
Tomek Kulczyński <kulczynski.t@gmail.com>
2019-03-14 21:21:02 +0100

    finished task zadanie01

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,14 @@
-//Twój kod
+const express = require('express');
+const app = express();
+
+/* Routing z parametrem */
+app.get('/:firstnumber/:secondnumber', (req, res) => {
+    const firstNumber = req.params.firstnumber;
+    const secondNumber = req.params.secondnumber;
+    const sum = Number(firstNumber) + Number(secondNumber);
+    res.send('Wynik to ' + sum);
+})
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
