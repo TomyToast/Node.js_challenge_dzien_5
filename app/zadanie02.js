efdee63596e1b054db101e6e7f0b6a28bc6ed5d3
Tomek Kulczyński <kulczynski.t@gmail.com>
2019-03-14 21:33:37 +0100

    finished task zadanie02

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,26 @@
-//Twój kod
+const express = require('express');
+const app = express();
+
+let imie = '';
+
+app.get('/name/set/:imie', (req, res) => {
+    imie = req.params.imie;
+
+    res.send('Imie to: ' + imie);
+});
+
+app.get('/name/show', (req, res) => {
+        res.send('Sprawdzilem, masz na imie: ' + imie);
+});
+
+app.get('/name/check', (req, res) => {
+    if(!imie){
+        res.send('To imie nie zostało zapisane');
+    } else {
+        res.send('To imie zostało zapisane');
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
