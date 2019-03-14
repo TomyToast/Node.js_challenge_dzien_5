dc08d3ed42a28e106d6499de832eb63f04a87c0b
Tomek Kulczyński <kulczynski.t@gmail.com>
2019-03-14 21:52:18 +0100

    finished task zadanieDnia1

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,25 @@
-//Twój kod
+const express = require('express');
+const app = express();
+
+let yes = 0;
+let no = 0;
+
+app.use(express.static('./public/zadanieDnia'));
+
+app.get('/vote/yes', (req, res) => {
+    yes++;
+    res.send('<a href="/">dziękujemy za głos!</a>')
+})
+
+app.get('/vote/no', (req, res) => {
+    no++;
+    res.send('<a href="/">dziękujemy za głos!</a>')
+})
+
+app.get('/check', (req, res) => {
+    res.send('Tak: '+ yes + ' Nie: '+ no + ' <a href="/">Back</a>');
+})
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
